3e23beb92afbd2428e707c970be0bd38335980df
Doko <doraemonfanclub@gmail.com>
2023-02-14 11:43:00 +0700

    Add dummy

---
+++ src/status_im/ui/screens/about_app/dummy_doodle.cljs
@@ -0,0 +1,9 @@
+(ns status-im.ui.screens.about-app.dummy-doodle
+  (:require [re-frame.core :as re-frame]
+            [status-im.i18n.i18n :as i18n]
+            [quo.core :as quo]
+            [status-im.ui.components.react :as react])
+
+  (:require-macros [status-im.utils.views :as views]))
+
+(defn dummy-doodle [test-prop1 & [optional-thing?]])
